9e78676410344ae97908d4594c222c185a7888ad
Krzysztof Pacholak <pacholak@gmail.com>
2021-03-04 13:30:09 +0100

    add imagePicker and set picked image to imageView, add present func to cameraButtonPressed

--- FlowerID/ViewController.swift
+++ FlowerID/ViewController.swift
@@ -17,10 +17,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         
         imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
+        
     
     }
 
+    // func to pick the image from camera
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let userPickedImage = info[UIImagePickerController.InfoKey.editedImage]
+        imageView.image = userPickedImage as? UIImage
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     
 }
